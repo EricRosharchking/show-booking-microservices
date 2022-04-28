0e96e95c273db98841dfc63e36bcd10a646cd216
EricRosharchking <liluoke9@gmail.com>
2022-04-28 22:14:12 +0800

    Add Exception classes for cli app

---
+++ booking-cli/src/main/java/com/liyuan/hong/showbooking/exception/AdminException.java
@@ -0,0 +1,10 @@
+package com.liyuan.hong.showbooking.exception;
+
+public class AdminException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5729526968400121313L;
+
+}
---
+++ booking-cli/src/main/java/com/liyuan/hong/showbooking/exception/BuyerException.java
@@ -0,0 +1,11 @@
+package com.liyuan.hong.showbooking.exception;
+
+public class BuyerException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4217181798442836646L;
+
+
+}
